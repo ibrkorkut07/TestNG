64d29922b4e3b3de2634fe88edd824a836cebc50
ibrkorkut07 <http://github.com/ibrkorkut07>
2022-11-09 20:47:03 +0100

    Dropdown

---
+++ src/test/java/tests/basics/C01_HandleDropdowns.java
@@ -0,0 +1,45 @@
+package tests.basics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class C01_HandleDropdowns {
+
+    WebDriver driver;
+    @Test
+    public void dropdownTest() {
+        WebElement dropdownElement = driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
+        Select select = new Select(dropdownElement);
+        select.selectByVisibleText("Books");
+        System.out.println(select.getFirstSelectedOption().getText());
+        List<WebElement> optionList = select.getOptions();
+
+        for (WebElement each: optionList) {
+            System.out.println(each.getText());
+        }
+    }
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.amazon.com/");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/tests/basics/C02_Dropdown2.java
@@ -0,0 +1,79 @@
+package tests.basics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class C02_Dropdown2 {
+
+    WebDriver driver;
+    Select select;
+    WebElement dropdownElement;
+    //  https://the-internet.herokuapp.com/dropdown adresine gidin.
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://the-internet.herokuapp.com/dropdown");
+        dropdownElement = driver.findElement(By.xpath("//select[@id='dropdown']"));
+        select = new Select(dropdownElement);
+    }
+
+    //  Index kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
+    @Test
+    public void indexTest() {
+        select.selectByIndex(1);
+        System.out.println(select.getFirstSelectedOption().getText());
+    }
+    //  Value kullanarak Seçenek 2'yi (Option 2) seçin ve yazdırın
+    @Test
+    public void valueTest() {
+        select.selectByValue("1");
+        System.out.println(select.getFirstSelectedOption().getText());
+    }
+    //  Visible Text (Görünen metin) kullanarak Seçenek 1’i (Option 1) seçin ve yazdırın
+    @Test
+    public void visibilityTest() {
+        select.selectByVisibleText("Option 1");
+        System.out.println(select.getFirstSelectedOption().getText());
+    }
+    //  Tüm dropdown değerleri(value) yazdırın
+    @Test
+    public void dropdownListTest() {
+        List<WebElement> optionList = select.getOptions();
+        for (WebElement each: optionList) {
+            System.out.println(each.getText());
+        }
+
+    }
+    //  Dropdown’un boyutunu bulun, Dropdown’da 4 öğe varsa konsolda True, degilse False yazdırın
+    @Test
+    public void dropdownSizeTest() {
+
+        List<WebElement> optionList = select.getOptions();
+        int size = optionList.size();
+        System.out.println(size);
+        if (size==4) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
